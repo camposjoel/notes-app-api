2403fca3f910d373738a361da483b48cd780a452
Joel Campos <underkillr@gmail.com>
2025-12-03 18:16:39 -0600

    add vercel settings and app initiation config

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,13 @@ import { swagger } from "@elysiajs/swagger";
 import { auth } from "./auth";
 import { notes } from "./notes";
 
-const app = new Elysia()
+export default new Elysia()
   .use(swagger())
   .use(auth)
   .use(notes)
   .get("/", () => "Hello Notes")
-  .listen(3000);
+//.listen(3000);
 
-console.log(
-  `Notes API is running at ${app.server?.hostname}:${app.server?.port} 🚀`
-);
+// console.log(
+//   `Notes API is running at ${app.server?.hostname}:${app.server?.port} 🚀`
+// );
